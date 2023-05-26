1be8eab93a4a12a46cb0501801680da46de5e76e
Aspandiyar933 <ggg.megadoom.lol@gmail.com>
2023-05-27 01:03:33 +0600

    one

---
+++ Assets/Scripts/LoseLife.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoseLife : MonoBehaviour
+{
+    [SerializeField] private GameManager gameManager;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        gameManager.RemoveLife();
+    }
+}
